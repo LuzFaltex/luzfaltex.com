f3a24d174d8527998a0e5317cf852d7de4fef268
Foxtrek_64 <devin.duanne@Luzfaltex.com>
2021-01-13 04:12:31 -0600

    Initial push

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+using Statiq.Common;
+
+namespace LuzFaltex.Web
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+            => await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            /*
+            .DeployToGitHubPagesBranch(
+                "LuzFaltex", 
+                "www.luzfaltex.com", 
+                Config.FromSetting<string>("GITHUB_TOKEN"),
+                "gh_pages") */
+            .RunAsync();
+    }
+}
